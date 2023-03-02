044aafb93fc37de52baf5d7497c1353b731ab7c7
sammiearchie77 <chinonsosamuel29@gmail.com>
2023-03-01 23:31:30 -0800

    Debugging

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -22,11 +22,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	if (new->parent == NULL) /* root */
 		return new;
-
 	if (new->n < parent->n)
 		parent->left = new;
 	else
 		parent->right = new;
-
 	return new;
 }
